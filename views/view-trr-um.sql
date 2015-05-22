7e6aa1e8cae25c8af606e02216943b4b6370448b
Tim Markello <timmarkello@gmail.com>
2015-05-22 09:30:49 -0500

    + add in unittype code and unittype name + add calculation of hours for standard and high need tracks when the service type is 'HOURS'.    :: todo need something to handle EVENT and DOLLAR unit types

--- views/view-trr-um.sql
+++ views/view-trr-um.sql
@@ -16,10 +16,20 @@ select spm.ServicePackageMapId
 	, st.Name [ServiceType]
 	, spm.ServiceId
 	, s.Name [Service]
+	, s.UnitType
+	, sut.Name [UnitTypeName]
 	, spm.StandardUnits
 	, spm.HighNeedUnits
-	--, sm.ServiceActivityCode, sac.UOSFee
 	, avg(cast(sac.UOSFee as float)) [AvgStdFee]
+	, case when s.unittype = 1 /*unittype = hours*/
+	     then spm.StandardUnits * s.ValuePerUnit
+	     else null
+	  end [StandardHours]
+	, case when s.unittype = 1 /*unittype = hours*/
+	     then spm.HighNeedUnits * s.ValuePerUnit
+	     else null
+	end [HighNeedHours]
+	--, sm.ServiceActivityCode, sac.UOSFee
 	, spm.StandardUnits * avg(cast(sac.UOSFee as float)) [StandardDollars]
 	, spm.HighNeedUnits * avg(cast(sac.UOSFee as float)) [HighDollars]
 	, sp.effectivedate
@@ -31,6 +41,8 @@ join trr_um_service_type st
   on st.ServiceTypeId = spm.ServiceTypeId
 join trr_um_service s
   on s.ServiceId = spm.ServiceId
+join trr_um_service_unit_type sut
+  on s.unittype = sut.serviceunittypeid
 join trr_um_service_sac_map sm
   on sm.ServiceId = s.serviceid
 join SysFile.SAC sac
@@ -40,7 +52,11 @@ group by sp.code
 	, spm.ServicePackageId
 	, sp.Name, spm.ServiceTypeId
 	, st.Name, spm.ServiceId
-	, s.Name, spm.StandardUnits
+	, s.Name
+	, s.unittype
+	, sut.Name
+	, s.ValuePerUnit
+	, spm.StandardUnits
 	, spm.HighNeedUnits
 	, sp.effectivedate
 	, sp.expirationdate
